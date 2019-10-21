52d69eb1d07c143f21098743f8cf48c17bf89626
Scott Sauceda <msauceda0812@gmail.com>
2019-10-21 14:21:25 -0400

    Added Post and Comment Entity

---
+++ src/com/ga/entity/Post.java
@@ -0,0 +1,66 @@
+package com.ga.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column
+    private String title;
+
+    @Column
+    private String postText;
+
+    public Post() {}
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "user_post",
+            joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+
+}
