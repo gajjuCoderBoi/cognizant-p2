eb7098457679997e26dc1ae227dc583ba677ac66
Mohammad Javed <ghazanfar9131@gmail.com>
2019-10-22 19:59:48 -0400

    - Fixed JwtResponse Object. -

--- src/com/ga/entity/JwtResponse.java
+++ src/com/ga/entity/JwtResponse.java
@@ -21,24 +21,8 @@ public class JwtResponse {
         return this.jwt;
     }
 
-    public JwtResponse(String jwt, String username) {
-        this.jwt = jwt;
-        this.username = username;
-    }
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
-
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
